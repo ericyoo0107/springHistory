25bd16d699a00cc563d473e0bb081d59b1ed1439
YuJunHyuk <ericyoo0107@gmail.com>
2022-12-18 02:22:08 +0900

    refactor : 어뎁터라는 개념을 사용해서 v3와 v4 둘다 사용할수 있게 만듬 v4의 컨트롤러는 반환값이 string 형태인데 modelview를 반환하도록 어뎁터 만듬

---
+++ servlet/src/main/java/hello/servlet/web/servlet/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.servlet.frontcontroller.v5.adapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import hello.servlet.web.servlet.frontcontroller.ModelView;
+import hello.servlet.web.servlet.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.servlet.frontcontroller.v5.MyHandlerAdapter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+	@Override
+	public boolean supports(Object handler) {
+		return (handler instanceof ControllerV3);
+	}
+
+	@Override
+	public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		ControllerV3 controller = (ControllerV3)handler;
+		Map<String, String> paramMap = createParamMap(request);
+		ModelView mv = controller.process(paramMap);
+		return mv;
+	}
+
+	private Map<String, String> createParamMap(HttpServletRequest request) {
+		Map<String, String> paramMap = new HashMap<>();
+		request.getParameterNames().asIterator()
+			.forEachRemaining(paramName -> paramMap.put(paramName,
+				request.getParameter(paramName)));
+		return paramMap;
+	}
+}
